406257f8bb641f878bb8b993f6d62996dfb697d2
krishan kumar <kishanadlakha@gmail.com>
2025-08-19 13:34:45 +0530

    bucket name was incorrect terraform-ec2-state-kishan

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
 	backend "s3" {
-		bucket		=	"terraform-kishan-tf-state"
+		bucket		=	"terraform-ec2-state-kishan"
 		key		=	"ec2-template/terraform.tfstate"
 		region		=	"eu-north-1"
 		encrypt		=	true
