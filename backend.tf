9f4e94dd34806671d816a75ae0f2ead9b3ccf739
krishan kumar <kishanadlakha@gmail.com>
2025-08-19 13:28:06 +0530

    region word was wrong in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
 	backend "s3" {
 		bucket		=	"terraform-kishan-tf-state"
 		key		=	"ec2-template/terraform.tfstate"
-		regiom		=	"eu-north-1"
+		region		=	"eu-north-1"
 		encypt		=	true
 		}
 	}
